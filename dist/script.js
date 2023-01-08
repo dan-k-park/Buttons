e1d1297ae3fd6297cd9ca242ab5f11ac7d8a4b3c
dan-k-park <daniel.kun.park@gmail.com>
2023-01-07 20:30:50 -1000

    another hamburger menu

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,8 @@
 const menuBtn = document.querySelector('.hamburger-menu');
+const menuBBtn = document.querySelector('.hamburger-menuB');
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
+});
+menuBBtn.addEventListener('click', () => {
+  menuBBtn.classList.toggle('open');
 });
