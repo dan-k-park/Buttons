39c2e699656f6bc8361c49f8c70ce1e42091d820
dan-k-park <daniel.kun.park@gmail.com>
2023-04-11 15:52:09 -1000

    accordian

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,19 @@ menuBtn.addEventListener('click', () => {
 menuBBtn.addEventListener('click', () => {
   menuBBtn.classList.toggle('open');
 });
+
+var acc = document.getElementsByClassName('accordian');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    console.log(panel.style);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+}
